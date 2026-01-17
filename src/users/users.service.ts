10a5bb95556c535584e53ba282c2fab3b9199183
the-anshu-dev <anshuchaurasia210@gmail.com>
2026-01-18 01:30:54 +0530

    refactor: genericize role population logic and apply to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ import { Role, RoleDocument } from '../schemas/role.schema';
 import { CreateUserDto } from './dto';
 import { BaseService } from '../common/base/base.service';
 import { PasswordUtil } from '../common/utils';
+import { populateUserRoles } from '../common/utils/rolePopulat.util';
 
 @Injectable()
 export class UsersService extends BaseService<UserDocument> {
@@ -67,6 +68,8 @@ export class UsersService extends BaseService<UserDocument> {
     }
   }
 
+
+
   async findAll(options: {
     filter?: any;
     select?: string;
@@ -82,97 +85,7 @@ export class UsersService extends BaseService<UserDocument> {
     const allUsers = await this.model.find({}).lean().exec();
 
     // 2. Populate Roles (Manual Population)
-    const numericRoles = new Set<number>();
-    const stringRoles = new Set<string>();
-    const objectIdRoles = new Set<string>();
-
-    allUsers.forEach((user: any) => {
-      const roles = Array.isArray(user.role)
-        ? user.role
-        : user.role
-          ? [user.role]
-          : [];
-      roles.forEach((r: any) => {
-        if (typeof r === 'number') {
-          numericRoles.add(r);
-        } else if (typeof r === 'string') {
-          const num = Number(r);
-          if (!isNaN(num)) {
-            numericRoles.add(num);
-          } else if (r.match(/^[0-9a-fA-F]{24}$/)) {
-            objectIdRoles.add(r);
-          } else {
-            stringRoles.add(r);
-          }
-        } else if (r && r.toString().match(/^[0-9a-fA-F]{24}$/)) {
-          // Handle ObjectId objects if lean() returned them as such
-          objectIdRoles.add(r.toString());
-        }
-      });
-    });
-
-    const roleMap = new Map<any, any>();
-    const tokenPromises: Promise<any>[] = [];
-
-    if (numericRoles.size > 0) {
-      tokenPromises.push(
-        this.userRoleModel
-          .find({ role_type: { $in: Array.from(numericRoles) } })
-          .lean()
-          .exec(),
-      );
-    }
-    if (stringRoles.size > 0) {
-      tokenPromises.push(
-        this.userRoleModel
-          .find({ key: { $in: Array.from(stringRoles) } })
-          .lean()
-          .exec(),
-      );
-    }
-    if (objectIdRoles.size > 0) {
-      tokenPromises.push(
-        this.userRoleModel
-          .find({ _id: { $in: Array.from(objectIdRoles) } })
-          .lean()
-          .exec(),
-      );
-    }
-
-    const fetchedRolesGroups = await Promise.all(tokenPromises);
-    fetchedRolesGroups.flat().forEach((role: any) => {
-      if (role.role_type) roleMap.set(role.role_type, role);
-      if (role.key) roleMap.set(role.key, role);
-      if (role._id) roleMap.set(role._id.toString(), role);
-    });
-
-    allUsers.forEach((user: any) => {
-      const userRoles = Array.isArray(user.role)
-        ? user.role
-        : user.role
-          ? [user.role]
-          : [];
-      user.role = userRoles.map((r: any) => {
-        let found = roleMap.get(r);
-
-        // Try looking up by numeric value if string
-        if (!found && typeof r === 'string' && !isNaN(Number(r))) {
-          found = roleMap.get(Number(r));
-        }
-
-        // Try looking up by string if object (ObjectId)
-        if (!found && typeof r === 'object' && r.toString) {
-          found = roleMap.get(r.toString());
-        }
-
-        // Try looking up by string if it looks like an ID but wasn't found directly (redundant if first get worked, but safe)
-        if (!found && typeof r === 'string' && r.match(/^[0-9a-fA-F]{24}$/)) {
-          found = roleMap.get(r);
-        }
-
-        return found || r;
-      });
-    });
+    await populateUserRoles(this.userRoleModel, allUsers);
 
     // 3. In-Memory Filtering
     let filteredUsers = allUsers;
@@ -279,6 +192,24 @@ export class UsersService extends BaseService<UserDocument> {
 
   async findByPhone(phone_number: string): Promise<UserDocument | null> {
     return this.model.findOne({ phone_number }).exec();
+  }
+
+  async findAdmins(): Promise<UserDocument[]> {
+    const admins = await this.model.find({ role: { $in: [1, 2] } }).lean().exec();
+    await populateUserRoles(this.userRoleModel, admins);
+    return admins as any;
+  }
+
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.model.findOne({ email }).exec();
+  }
+
+  async findByIdentifier(identifier: string): Promise<UserDocument | null> {
+    // Try to find by email first, then by phone
+    const user = await this.model.findOne({
+      $or: [{ email: identifier }, { phone_number: identifier }],
+    }).exec();
+    return user;
   }
 
   // async verifyPassword(
